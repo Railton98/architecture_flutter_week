8ea89e57b0d1df123df0dbecab0063a9b94e8e85
Railton98 <railtonleal98@gmail.com>
2020-05-28 01:08:36 -0300

    lesson #3 - Service

--- lib/app/app_controller.dart
+++ lib/app/app_controller.dart
@@ -1,17 +1,28 @@
+import 'package:architecture_flutter_week/app/services/shared_local_storage_service.dart';
 import 'package:flutter/material.dart';
 
+import 'interfaces/local_storage_interface.dart';
 import 'models/app_config_model.dart';
 
 class AppController {
   static final AppController instance = AppController._();
 
-  AppController._();
+  AppController._() {
+    storage.get('isDark').then((value) {
+      if (value != null) {
+        config.themeSwitch.value = value;
+      }
+    });
+  }
 
   final AppConfigModel config = AppConfigModel();
-
   bool get isDark => config.themeSwitch.value;
-
   ValueNotifier<bool> get themeSwitch => config.themeSwitch;
 
-  changeTheme(bool value) => config.themeSwitch.value = value;
+  final LocalStorageInterface storage = SharedLocalStorageService();
+
+  changeTheme(bool value) {
+    config.themeSwitch.value = value;
+    storage.put('isDark', value);
+  }
 }
